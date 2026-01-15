d93a688bdcd02bffd7cf0435e50fea7446087fa5
ivann <ivanng2121@gmail.com>
2026-01-15 10:20:47 -0500

    mean reversion

---
+++ strat.py
@@ -0,0 +1,55 @@
+import requests
+import csv
+import numpy as np
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo'
+r = requests.get(url)
+data = r.json()
+
+dates = data['Time Series (Daily)'].keys()
+Foo = []
+
+for date in dates:
+    Foo.append([date, 'IBM', float(data['Time Series (Daily)'][date]['5. adjusted close'])])
+
+Foo = Foo[:100]
+Foo.reverse()
+
+for i in range(1, 100):
+    Foo[i].append(np.log(Foo[i][2]) - np.log(Foo[i-1][2]))
+
+ma = []
+for i in range(100):
+    ma.append(Foo[i][2])
+    if i > 2:
+        ma.pop(0)
+    if i > 1:
+        Foo[i].append(np.mean(ma))
+    
+for i in range(3, 100):
+    if Foo[i][2] > Foo[i-1][4]:
+        Foo[i].append(1)
+    else:
+        Foo[i].append(0)
+
+for i in range(3, 99):
+    if Foo[i][5] == 1:
+        Foo[i].append(Foo[i+1][2]-Foo[i][2])
+    else:
+        Foo[i].append(0)
+Foo[99].append(0)
+
+with open("3_Day.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerows(Foo)
+
+returns = 0
+for i in range(3, 100):
+    returns += Foo[i][6]
+
+annualized_return = (returns * 365 / 100)
+
+print("3 Day Moving Average", returns, "Annualized Return:", annualized_return)
+
+
